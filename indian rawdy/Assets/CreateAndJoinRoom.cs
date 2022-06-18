4f16a13898444278bac5e2323677f5d0cc1fec78
kushwahadeepesh <86400206+kushwahadeepesh@users.noreply.github.com>
2022-06-18 15:43:51 +0530

    player baki

---
+++ indian rawdy/Assets/CreateAndJoinRoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
+{
+
+    public InputField CreateInput;
+    public InputField joinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(CreateInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+}
---
+++ indian rawdy/Assets/Spawnplayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Spawnplayer : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+
+    public float MinX;
+    public float MaxX;
+    public float MinY;
+    public float MaxY;
+    public float MinZ;
+    public float MaxZ;
+
+    private void Start()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range(MinX, MaxX), Random.Range(MinY, MaxY), Random.Range(MinZ, MaxZ));
+        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+    }
+
+}
